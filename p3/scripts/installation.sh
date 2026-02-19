da8adab5c3874597ce5041c018081a4379df1172
liperez <liperez@IoT.myguest.virtualbox.org>
2026-02-19 17:15:12 +0100

    Script : have access to argo cd & v1 and v2 good, curl is ok but no endpoints visible on argocd

--- p3/scripts/installation.sh
+++ p3/scripts/installation.sh
@@ -22,10 +22,10 @@ sudo rm -rf ~/.kube/config
 sudo rm -rf /etc/kubernetes
 
 echo "Suppression des ressources ArgoCD..."
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+sudo kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 echo "Suppression de K3D..."
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash -s --uninstall
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash -s --uninstall
 
 echo "Nettoyage des fichiers temporaires..."
 sudo apt autoremove -y
@@ -40,49 +40,59 @@ echo "------------ Nettoyage terminé ------------"
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
+echo "apres update"
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Docker installation succeeded !"
 
 #Start Docker
+echo "Starting docker..."
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker --no-pager
 
-#Adduser
-sudo usermod -aG docker $USER
-newgrp docker
-
 #Install k3d
-echo "lala"
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+echo "Installing k3d..."
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 #Install  kubectl
-echo "ici"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+echo "Installing kubectl..."
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 #Create cluster
-echo "ouioui"
-sudo k3d cluster create IOT-cluster --port "8080:80@loadbalancer" --port "8888:8888@loadbalancer" --wait
+echo "Creating cluster..."
+sudo k3d cluster create IOT-cluster --port "8888:8888@loadbalancer" --wait
 sleep 10
 
 #Create namespaces
+echo "Creating namespaces..."
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 
+rm -rf ~/.kube/config
 mkdir -p ~/.kube
 k3d kubeconfig get IOT-cluster > ~/.kube/config
 chmod 600 ~/.kube/config
 
-kubectl config set-cluster k3d-IOT-cluster --server=https://localhost:35409 --insecure-skip-tls-verify=true
-
 #Install ArgoCD in the namespaces (server-side)
 sudo kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo "Argocd installation succeeded !"
 
 #wait pods Argo CD
+echo "Waiting for the pods to be ready..."
 sudo kubectl wait --for=condition=Ready pods -n argocd --all --timeout=300s
 
-kubectl port-forward svc/argocd-server -n argocd 8888:443
+#Apply application
+sudo kubectl apply -f ../confs/application.yaml -n argocd
+
+#Connect to argocd via port 8080
+echo "Starting the server connection..."
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+
+#Get admin password
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
