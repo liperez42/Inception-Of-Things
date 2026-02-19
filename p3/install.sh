718898976d9344df6271b1d42238d5f7ebd59875
sfraslin <sfraslin@VM-IoT.myguest.virtualbox.org>
2026-02-19 17:06:31 +0100

    script

--- p3/install.sh
+++ p3/install.sh
@@ -2,106 +2,85 @@
 
 # ----- Clean Start ----- #
 
-echo "Démarrage du nettoyage des installations précédentes..."
+echo "Cleaning previous installations..."
 
-# 1. Supprimer les conteneurs Docker et les configurations
-echo "Suppression de Docker et de ses dépendances..."
-sudo systemctl stop docker
+# 1. Docker
+echo "Deleting Docker and depedancies..."
+sudo systemctl stop docker 2>/dev/null
 sudo dpkg --configure -a
-sudo apt remove --purge -y docker-ce docker-ce-cli containerd.io
+sudo apt remove --purge -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
-# 2. Supprimer les clusters K3D existants
-echo "Suppression des clusters K3D..."
-sudo k3d cluster list --no-headers | awk '{print $1}' | xargs -I {} sudo k3d cluster delete {}
+# 2. K3D
+echo "Deleting K3D clusters..."
+k3d cluster delete --all 2>/dev/null
 
-# 3. Supprimer les namespaces Kubernetes
-echo "Suppression des namespaces Kubernetes..."
-sudo kubectl delete namespace argocd dev --ignore-not-found
+# 3. Kubernetes
+echo "Deleting Kubernetes configuration files..."
+rm -rf ~/.kube/config
 
-# 4. Supprimer les fichiers de configuration Kubernetes
-echo "Suppression des fichiers de configuration Kube..."
-sudo rm -rf ~/.kube/config
-sudo rm -rf /etc/kubernetes
+echo "------------ Cleaning done ------------"
 
-# 5. Supprimer ArgoCD
-echo "Suppression des ressources ArgoCD..."
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+#!/bin/bash
 
-# 6. Désinstallation de Kubernetes et K3D
-echo "Suppression de K3D..."
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash -s --uninstall
-
-# 7. Nettoyage des fichiers résiduels
-echo "Nettoyage des fichiers temporaires..."
-sudo apt autoremove -y
-sudo apt clean
-
-echo "Nettoyage terminé."
-echo "------------ Nettoyage terminé ------------"
-
-# Installe les dépendances
+#Install Docker
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
-
-# Ajoute la clé GPG officielle de Docker
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Ajoute le dépôt Docker pour Debian Bookworm
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-# Installe Docker Engine
 sudo apt update
+echo "apres update"
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Docker installation succeeded !"
 
-sleep 10
-
-#Redemarre Docker
-sudo systemctl restart docker
+#Start Docker
+echo "Starting docker..."
 sudo systemctl enable docker
-echo "COUCOU COUCOU"
+sudo systemctl start docker
+sudo systemctl status docker --no-pager
 
-#Adduser
-sudo usermod -aG docker $USER
-# newgrp docker
-echo "HAHAHAHAHAHAHAHAHAH"
+#Install k3d
+echo "Installing k3d..."
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-#Reinstalle k3d
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+#Install  kubectl
+echo "Installing kubectl..."
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-echo "LALILALA"
-sleep 10
-
-#Cree le cluster k3d
-echo "Suppression du cluster K3D..."
-sudo k3d cluster delete test >> /tmp/k3d_log.txt 2>&1
-sleep 10 
-echo "Création du cluster K3D..."
-
-echo "Vérification du fichier de log"
-ls -l /tmp/k3d_log.txt
-
-while ! kubectl cluster-info > /dev/null 2>&1; do
-  echo "En attente de l'initialisation du cluster..."
-  sleep 5
-done
-
-#Configure kubectl
-k3d kubeconfig get IOT-cluster > ~/.kube/config
-
+#Create cluster
+echo "Creating cluster..."
+sudo k3d cluster create IOT-cluster --port 8888:8888@loadbalancer --wait
 sleep 10
 
 #Create namespaces
+echo "Creating namespaces..."
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 
-sleep 10
+rm -rf ~/.kube/config
+mkdir -p ~/.kube
+k3d kubeconfig get IOT-cluster > ~/.kube/config
+chmod 600 ~/.kube/config
 
 #Install ArgoCD in the namespaces (server-side)
 sudo kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo "Argocd installation succeeded !"
 
 #wait pods Argo CD
+echo "Waiting for the pods to be ready..."
 sudo kubectl wait --for=condition=Ready pods -n argocd --all --timeout=300s
+
+#Apply application
+sudo kubectl apply -f application.yaml -n argocd
+
+#Connect to argocd via port 8080
+echo "Starting the server connection..."
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+
+#Get admin password
+#kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
