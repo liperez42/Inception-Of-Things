13e18da8142a4ef088f5021f68eb491f61c669b2
sfraslin <sfraslin@VM-IoT.myguest.virtualbox.org>
2026-03-03 20:41:31 +0100

    bonus almost done

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -7,7 +7,6 @@ NC='\033[0m' # No Color
 
 #Get gitlab password
 source /tmp/gitlab_vars.sh
-echo -e "${GREEN}>>>>>> Gitlab root password: $GITLAB_PSW <<<<<<${NC}"
 
 # Checking Gitab
 echo -e "${BLUE}Checking Gitab is up and running...${NC}"
@@ -33,21 +32,21 @@ echo -e "${BLUE}Creating user sfraslin...${NC}"
 curl -s --request POST "http://localhost/api/v4/users" \
   --header "PRIVATE-TOKEN: $ROOT_TOKEN" \
   --data "name=Saina Fraslin&username=sfraslin&email=sfraslin@gitlab.local&password=Andr1amampandry!&skip_confirmation=true" > /dev/null
-echo -e "${GREEN}Utilisateur sfraslin créé !${NC}"
+echo -e "${GREEN}User sfraslin created !${NC}"
 
 # Creating repo
 echo -e "${BLUE}Creating repository...${NC}"
 curl -s --request POST "http://localhost/api/v4/projects" \
   --header "PRIVATE-TOKEN: $ROOT_TOKEN" \
   --data "name=sfraslin&visibility=public" > /dev/null
-echo -e "${GREEN}Repo créé !${NC}"
+echo -e "${GREEN}Repository created !${NC}"
 
 # Adding files
 echo -e "${BLUE}Adding files...${NC}"
 TEMP_DIR=$(mktemp -d)
-cp $PWD/bonus/gitlab_files/deployment.yaml $TEMP_DIR/
-cp $PWD/bonus/gitlab_files/service.yaml $TEMP_DIR/
-cp $PWD/bonus/gitlab_files/.gitlab-ci.yaml $TEMP_DIR/
+cp /home/sfraslin/gitlab_files/deployment.yaml $TEMP_DIR/
+cp /home/sfraslin/gitlab_files/service.yaml $TEMP_DIR/
+cp /home/sfraslin/gitlab_files/.gitlab-ci.yml $TEMP_DIR/
 
 cd $TEMP_DIR
 git init
@@ -59,4 +58,17 @@ git remote add origin http://root:$ROOT_TOKEN@localhost/root/sfraslin.git
 git push --set-upstream origin master:main
 cd -
 rm -rf $TEMP_DIR
-echo -e "${GREEN}Fichiers poussés !${NC}"
+echo -e "${GREEN}Files pushed !${NC}"
+
+RUNNER_TOKEN=$(curl -s --request POST "http://localhost/api/v4/user/runners" \
+  --header "PRIVATE-TOKEN: $ROOT_TOKEN" \
+  --data "runner_type=instance_type&description=k3d-runner" \
+  | jq -r '.token')
+
+helm install --namespace gitlab gitlab-runner \
+  --set gitlabUrl='http://gitlab-webservice-default.gitlab.svc.cluster.local:8181' \
+  --set runnerToken=$RUNNER_TOKEN \
+  --set rbac.create='true' \
+  gitlab/gitlab-runner
+
+sudo ./bonus/scripts/argocd.sh
