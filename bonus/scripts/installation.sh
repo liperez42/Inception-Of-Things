911f368210cf351c235952c10ffd660f25917186
sovincen <sovincen@student.42paris.fr>
2026-03-05 15:28:02 +0100

    cleaning in a separate script

--- bonus/scripts/installation.sh
+++ bonus/scripts/installation.sh
@@ -6,55 +6,7 @@ BLUE='\033[34m'
 NC='\033[0m' # No Color
 
 # ----- Clean Start -----
-echo -e "${GREEN}------------ Cleaning previous installations ------------${NC}"
-
-# Delete ArgoCD
-echo -e "${BLUE}Deleting ArgoCD...${NC}"
-if kubectl get ns argocd &> /dev/null; then
-  sudo kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-fi
-if k3d cluster list | grep -q "IOT-cluster"; then
-  sudo k3d cluster delete IOT-cluster
-fi
-
-# Uninstall gitlab
-echo -e "${BLUE}Uninstall gitlab...${NC}"
-if helm list -n gitlab &> /dev/null; then
-  helm uninstall gitlab --namespace gitlab
-fi
-
-# Delete Kubernetes namespaces
-echo -e "${BLUE}Deleting Kubernetes namespaces...${NC}"
-sudo kubectl delete namespace argocd dev gitlab --ignore-not-found
-
-# Uninstall Helm
-echo -e "${BLUE}Uninstall Helm...${NC}"
-sudo apt-get remove --purge helm -y
-sudo rm -f /usr/share/keyrings/helm.gpg
-sudo rm -f /etc/apt/sources.list.d/helm-stable-debian.list
-
-# Uninstall K3D
-echo -e "${BLUE}Uninstall K3D...${NC}"
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash -s --uninstall
-
-# Removing Docker
-echo -e "${BLUE}Deleting Docker...${NC}"
-if command -v helm &> /dev/null; then
-  sudo systemctl stop docker
-  sudo dpkg --configure -a
-  sudo apt remove --purge -y docker-ce docker-ce-cli containerd.io
-  sudo rm -rf /var/lib/docker
-  sudo rm -rf /var/lib/containerd
-fi
-
-# Apt clean
-echo -e "${BLUE}Cleaning apt...${NC}"
-sudo apt autoremove -y
-sudo apt clean
-
-echo -e "${GREEN}------------ Cleaning Done ------------${NC}"
-
-# exit
+sudo ./bonus/scripts/clean.sh
 
 # ----- Instalations -----
 
