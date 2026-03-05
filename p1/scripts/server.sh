262bca57d071de417d0f14c0c7c4f3210635dd23
sovincen <sovincen@student.42paris.fr>
2026-03-05 11:33:40 +0100

    adding write-kubconfig to the server script

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get upgrade
 apt-get install -y curl
 
-export INSTALL_K3S_EXEC="server --node-ip=192.168.56.110"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip=192.168.56.110"
 
 curl -sfL https://get.k3s.io | sh -
 
